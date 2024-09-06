fef2cff9d1f9a2bddb5678ca4546f731f76ffe4e
Timothy May <tmay@inscico.com>
2024-09-05 17:12:46 -0700

    Added comments to staticContentSettings.js

--- BlazorUI/wwwroot/staticContentSettings.js
+++ BlazorUI/wwwroot/staticContentSettings.js
@@ -1,13 +1,16 @@
 export default {
     staticAssets: [
+        // System
         { "/Assets/base/System/": "PreCache" },
         { "/Assets/en-US/System/": "PreCache" },
         { "/Assets/es-MX/System/": "LazyCache" },
 
+        // StoreApp
         { "Assets/base/StoreApp/": "PreCache" },
         { "/Assets/en-US/StoreApp/": "PreCache" },
         { "/Assets/es-MX/StoreApp/": "LazyCache" },
 
+        // Tenancy
         { "/Tenancy/base/StoreApp/": "PreCache" },
         { "/Tenancy/en-US/StoreApp/": "PreCache" },
         { "/Tenancy/es-MX/StoreApp/": "LazyCache" },
